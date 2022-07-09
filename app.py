fb14662b5b03340f840faa64438a30c0f8b39061
CARLOS MORALES <106545642+YosoyCa@users.noreply.github.com>
2022-07-09 12:59:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Aplicacion CM",page_icon="🦁")
 st.title('Aplicacion CM-HOUSE')
 
 st.sidebar.write('Nuestra App tiene en cuenta tu ritmo de vida y tus necesidades para ofrecerte infromacion puntual con base a tus necesidades de vivienda :')
-st.sidebar.write('**Datos de king country, USAsdfasfasfasfadf qwgwre 	werq werfqwef	 wef we fwe werwefwefwew we  **:')
+
 imagen = Image.open("unnamed.png")
 st.sidebar.image(imagen)
 st.write('**Datos de king country, USA **:')
